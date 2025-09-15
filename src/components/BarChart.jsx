853fdfe109807e48e1804d4d0ce9311a060ae087
Tannu Kannaojiya <tannukannaojiya@gmail.com>
2025-09-15 09:49:48 +0530

    new

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
-const BarChart = () => {
+const BarChartComponent = ({ data }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <ResponsiveContainer width='100%' height={300}>
+        <BarChart data={data} margin={{ top:50 }}>
+            <CartesianGrid strokeDasharray='10 10'/>
+            <XAxis dataKey='date'/>
+            <YAxis allowDecimals={false}/>
+            <Tooltip/>
+            <Bar dataKey='count' fill="#3b82f6" barSize={75}/>
+        </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
-export default BarChart
+export default BarChartComponent;
